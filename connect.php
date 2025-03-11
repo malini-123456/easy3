0adb4d0877eae400cca9e598d2345a3e0c547d9d
Ni Nyoman Sri Malini <easyttd@gmail.com>
2025-03-11 10:27:40 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $server = "localhost";
-// $username = "root";
-// $password = "";
-$username="malinieasy3";
-$password="2Anp^9z61";
+$username = "root";
+$password = "";
+// $username="malinieasy3";
+// $password="2Anp^9z61";
 $database = "easy3";
 $conn = mysqli_connect($server, $username, $password, $database) or die(mysqli_error($conn));
 // $db = mysql_select_db("php_crud",$conn) or die(mysql_error());
